3361f4c16d73d5d6f18d0cea7a9a1e0aa0d4a9e0
Yamaguchi <yamaguchi@YamaguchinoMacBook-Air.local>
2021-08-26 16:44:56 +0900

    add Enum

---
+++ Dart/Enum.dart
@@ -0,0 +1,15 @@
+
+void main() {
+
+  print(Country.AMERICA);
+  print(Country.AMERICA.toString());
+  print(Country.AMERICA.index);
+
+}
+
+enum Country {
+  JAPAN,
+  AMERICA,
+  GERMANY,
+  FRANCE
+}
