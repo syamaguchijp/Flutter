b2cb5d9e063f18deafdeec5d9f0e0f7b7a483f8b
Yamaguchi <yamaguchi@YamaguchinoMacBook-Air.local>
2021-09-01 17:50:36 +0900

    add tab

---
+++ App/bottom_tab/lib/main.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:io';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyWebView(),
+    );
+  }
+}
+
+class MyWebView extends StatefulWidget { // setStateを使う必要があるため、StatefulWidget
+
+  MyWebView({Key? key}) : super(key: key);
+
+  @override
+  _MyWebViewState createState() => _MyWebViewState();
+}
+
+class _MyWebViewState extends State<MyWebView> {
+
+  WebViewController? _webViewController;
+  bool _canGoBack = false;
+  bool _canGoForward = false;
+  int _position = 1;
+  bool _isLoading = false;
+  String _urlstring = _urlList[0];
+  int _selectedIndex = 0;
+
+  static List<String> _urlList = [
+    "https://yahoo.co.jp",
+    "https://apple.com",
+    "https://google.com"
+  ];
+
+  // タップ時の処理
+  void _onItemTapped(int index) {
+    print("_onItemTapped $index");
+    setState(() {
+      _selectedIndex = index;
+      _urlstring = _urlList[index];
+      print("_urlstring $_urlstring");
+      _webViewController?.loadUrl(_urlstring);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) {
+      WebView.platform = SurfaceAndroidWebView();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Text(_urlstring),
+      ),
+
+      body: _buildBody(),
+
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('Setting'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            title: Text('Search'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+
+      /*
+      // bottomNavigationBarと相性が悪く、上にずれるため、コメントアウトしておく
+      persistentFooterButtons: [
+        IconButton( // if (_canGoBack) で非表示にもできる
+          icon: Icon(Icons.arrow_back),
+          onPressed: _canGoBack ? _webViewController?.goBack : null,
+        ),
+        IconButton(
+          icon: Icon(Icons.arrow_forward),
+          onPressed: _canGoForward ? _webViewController?.goForward : null,
+        ),
+        IconButton(
+          icon: Icon(Icons.refresh),
+          onPressed: (){_webViewController?.reload();},
+        )
+      ],
+      */
+    );
+  }
+
+  Widget _buildBody() {
+
+    return Column(
+      children: [
+
+        if (_isLoading) const LinearProgressIndicator(),
+
+        Expanded( // Columnの子Widget間の隙間を目一杯埋める
+          child: IndexedStack( // 複数のwidgetの表示を切り替える場合、IndexedStackを使う
+            index: _position,
+            children: [
+              _buildWebView(),
+              Container(
+                color: Colors.white,
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildWebView() {
+
+    return WebView(
+      initialUrl: _urlstring,
+      javascriptMode: JavascriptMode.unrestricted,
+      onWebViewCreated: (controller) {
+        _webViewController = controller;
+      },
+      onPageStarted: (String url) {
+        setState(() {
+          _isLoading = true;
+          _position = 1;
+        });
+      },
+      onPageFinished: (String url) async {
+        if (_webViewController != null) {
+          _canGoBack = await _webViewController!.canGoBack();
+          _canGoForward = await _webViewController!.canGoForward();
+        }
+        setState(() {
+          _isLoading = false;
+          _position = 0;
+        });
+      },
+      onWebResourceError: (error) {
+        print("onWebResourceError : $error");
+      },
+    );
+  }
+}
--- App/webview/lib/main.dart
+++ App/webview/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import 'dart:async';
-import 'dart:io';
+import 'my_webview.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,125 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyWebView(urlstring: "https://yahoo.co.jp"),
     );
   }
-}
-
-class MyHomePage extends StatefulWidget { // setStateを使う必要があるため、StatefulWidget
-
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  WebViewController? _webViewController;
-  bool _canGoBack = false;
-  bool _canGoForward = false;
-  int _position = 1;
-  bool _isLoading = false;
-  String _title = '';
-  double _currentPositionX = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    if (Platform.isAndroid) {
-      WebView.platform = SurfaceAndroidWebView();
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-
-      appBar: AppBar(
-        title: Text(_title),
-      ),
-
-      body: _buildBody(),
-
-      persistentFooterButtons: [
-        IconButton( // if (_canGoBack) で非表示にもできる
-          icon: Icon(Icons.arrow_back),
-          onPressed: _canGoBack ? _webViewController?.goBack : null,
-        ),
-        IconButton(
-          icon: Icon(Icons.arrow_forward),
-          onPressed: _canGoForward ? _webViewController?.goForward : null,
-        ),
-        IconButton(
-          icon: Icon(Icons.refresh),
-          onPressed: (){_webViewController?.reload();},
-        )
-      ],
-    );
-  }
-
-  Widget _buildBody() {
-
-    return Column(
-      children: [
-
-        if (_isLoading) const LinearProgressIndicator(),
-
-        Expanded( // Columnの子Widget間の隙間を目一杯埋める
-          child: IndexedStack( // 複数のwidgetの表示を切り替える場合、IndexedStackを使う
-            index: _position,
-            children: [
-              _buildWebView(),
-              Container(
-                color: Colors.white,
-                child: Center(
-                  child: CircularProgressIndicator(),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildWebView() {
-
-    return WebView(
-      initialUrl: 'https://yahoo.co.jp',
-      javascriptMode: JavascriptMode.unrestricted,
-      onWebViewCreated: (controller) {
-        _webViewController = controller;
-      },
-      onPageStarted: (String url) {
-        setState(() {
-          _isLoading = true;
-          _position = 1;
-        });
-      },
-      onPageFinished: (String url) async {
-        if (_webViewController != null) {
-          _canGoBack = await _webViewController!.canGoBack();
-          _canGoForward = await _webViewController!.canGoForward();
-        }
-        setState(() {
-          _isLoading = false;
-          _position = 0;
-        });
-        final title = await _webViewController!.getTitle();
-        setState(() {
-          if (title != null) {
-            _title = title;
-          }
-        });
-      },
-      onWebResourceError: (error) {
-        print("onWebResourceError : $error");
-      },
-    );
-  }
-
 }
